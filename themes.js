0d920906c53a573773ada8348746ce4a2949da36
Mlyrr <mlyrr.github@pm.me>
2026-03-20 22:08:42 +0100

    separated project into 2 files

---
+++ themes.js
@@ -0,0 +1,21 @@
+const themes = {
+  bb8: {
+    name: "BB-8",
+    light: {
+      "--orange": "#FF7A1A",
+      "--orange-light": "#FFB26B",
+      "--blue": "#3EA6FF",
+      "--silver": "#C0C6CF",
+      "--bg": "#FFFFFF",
+      "--text": "#1F2933"
+    },
+    dark: {
+      "--orange": "#FF7A1A",
+      "--orange-light": "#FFB26B",
+      "--blue": "#3EA6FF",
+      "--silver": "#C0C6CF",
+      "--bg": "#0F172A",
+      "--text": "#F8FAFC"
+    }
+  }
+};
