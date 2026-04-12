7ec854323959eb9a11c4d6546fdd904567161920
Mlyrr <mlyrr.github@pm.me>
2026-04-12 18:07:05 +0200

    fixed stuff

--- themes.js
+++ themes.js
@@ -24,16 +24,16 @@ name: "Cherry Breeze",
 light: {
 "--primary": "#D7263D",
 "--primary-light": "#F28B9A",
-"--accent": "#74dfbb",
-"--neutral": "#6FCF97",
+"--accent": "#94f3cf",
+"--neutral": "#56c283",
 "--bg": "#FFFFFF",
 "--text": "#1F2933"
 },
 dark: {
 "--primary": "#FF4D6D",
 "--primary-light": "#FF8FA3",
-"--accent": "#74dfbb",
-"--neutral": "#4CAF7A",
+"--accent": "#94f3cf",
+"--neutral": "#56c283",
 "--bg": "#0B1220",
 "--text": "#F1F5F9"
 }
