df8ce6fee5d0ff79b9ceb846408ec4a01a099889
Mlyrr <mlyrr.github@pm.me>
2026-04-28 18:24:03 +0200

    added color pallet

--- themes.js
+++ themes.js
@@ -30,12 +30,33 @@ light: {
 "--text": "#1F2933"
 },
 dark: {
-"--primary": "#FF4D6D",
+"--primary": "#c41d4f",
 "--primary-light": "#FF8FA3",
 "--accent": "#94f3cf",
 "--neutral": "#56c283",
 "--bg": "#0B1220",
 "--text": "#F1F5F9"
 }
+},
+
+nebula_mix: {
+name: "Nebula",
+light: {
+"--primary": "#cb7cc0",
+"--primary-light": "#e8d1ea",
+"--accent": "#B388EB",
+"--neutral": "#A9CCFF",
+"--bg": "#F8FAFC",
+"--text": "#1F2933"
+},
+dark: {
+"--primary": "#8f2f92",
+"--primary-light": "#eb82c3",
+"--accent": "#246fdf",
+"--neutral": "#3f1f8f",
+"--bg": "#0D1B2A",
+"--text": "#E6EDF3"
 }
+}
+
 };
